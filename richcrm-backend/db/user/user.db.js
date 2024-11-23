69cfd4e6f5f340d96e4caeab32f5ea76ec001386
yuzchen7 <yuzchen7@gmail.com>
2024-11-22 21:09:12 -0500

    add: 4 flied added for verification

--- richcrm-backend/db/user/user.db.js
+++ richcrm-backend/db/user/user.db.js
@@ -9,6 +9,10 @@
  * @property {string} UserName - User's name
  * @property {enum} Role - User's role (ADMIN, ATTORNEY, CLIENT)
  * @property {string} UploadFolderName - User's upload folder name
+ * @property {string} Salt - User's salt for password encryption
+ * @property {string} refreshToken - User's renew token
+ * @property {string} VerificationCode - User's verification code
+ * @property {number} VerificationExp - User's verification expiration time
  */
 
 const db = require('../dynamodb');
@@ -43,6 +47,7 @@ class User {
             Item: {
                 EmailAddress: user.emailAddress,
                 Password: user.password,
+                Salt: user.salt,
                 UserName: user.userName,
                 Role: user.role,
                 UploadFolderName: user.uploadFolderName
@@ -67,22 +72,39 @@ class User {
         var updateExpressions = [];
         var expressionAttributeNames = {};
         // Optional fields
-        if (user.password !== undefined) {
+        if (user.password !== undefined && user.salt !== undefined) {
             updateExpressions.push("Password = :p");
+            updateExpressions.push("Salt = :s");
             params.ExpressionAttributeValues[':p'] = user.password;
+            params.ExpressionAttributeValues[':s'] = user.salt;
         }
+
         if (user.userName !== undefined) {
             updateExpressions.push("UserName = :u");
             params.ExpressionAttributeValues[':u'] = user.userName;
         }
+
         if (user.role !== undefined) {
             updateExpressions.push("#r = :r");
             expressionAttributeNames['#r'] = 'Role';
             params.ExpressionAttributeValues[':r'] = user.role;
         }
+
         if (user.uploadFolderName !== undefined) {
             updateExpressions.push("UploadFolderName = :ufn");
             params.ExpressionAttributeValues[':ufn'] = user.uploadFolderName;
+        }
+
+        if (user.verificationCode !== undefined && user.verificationExp !== undefined) {
+            updateExpressions.push("verificationCode = :v"),
+            updateExpressions.push("verificationExp = :e"),
+            params.ExpressionAttributeValues[':v'] = user.verificationCode;
+            params.ExpressionAttributeValues[':e'] = String(user.verificationExp.getTime());
+        }
+
+        if (user.refreshToken !== undefined) {
+            updateExpressions.push("RefreshToken = :t");
+            params.ExpressionAttributeValues[':t'] = user.refreshToken;
         }
 
         if (updateExpressions.length > 0) {
@@ -90,9 +112,11 @@ class User {
         } else {
             return null;
         }
+
         if (Object.keys(expressionAttributeNames).length > 0) {
             params.ExpressionAttributeNames = expressionAttributeNames;
         }
+
         const update = await db.update(params).promise();
         return update.Attributes;
     }
